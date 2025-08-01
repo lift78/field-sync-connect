8a19832e9c806515ab71cf9baac82057862e22ad
Giits25478 <168422980+Giits25478@users.noreply.github.com>
2025-08-01 11:58:44 +0300

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.da3c5a951a55424bb1b2b102a1fdb4b2',
-  appName: 'field-sync-connect',
+  appName: 'Lift Offline',
   webDir: 'dist',
   // REMOVE the server.url block completely
   plugins: {
